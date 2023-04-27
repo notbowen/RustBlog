9553a68008c76720a2d1e78a94b62b0f346a4253
wHo69 <hu_bowen@students.edu.sg>
2023-04-27 21:10:42 +0800

    Added smooth scrolling

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,27 @@
-const swup = new Swup();
+const options = {
+    animationSelector: '[class*="transition-fade"]',
+    animateHistoryBrowsing: true,
+    plugins: [
+        new SwupScrollPlugin({
+            animateScroll: true
+        })
+    ]
+};
+
+const swup = new Swup(options);
+
+let scrollValues = {};
+
+swup.on('clickLink', () => {
+    scrollValues[window.location.href] = window.scrollY;
+});
+
+swup.on('popState', () => {
+    setTimeout(function() {
+        window.scrollTo(0, scrollValues[window.location.href]);
+    }, 100);
+});
+
 swup.on("contentReplaced", () => {
     hljs.highlightAll();
 })

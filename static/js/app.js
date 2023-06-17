2655773cb1bc15bb8e092ad38c9701945e006d57
Hu Bowen <contact@hubowen.dev>
2023-06-17 17:35:35 +0800

    Added splide for swiping carousel + code formatting

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,60 @@
+// Swup.JS code
 const options = {
     animationSelector: '[class*="transition-fade"]',
     animateHistoryBrowsing: true,
     plugins: [
         new SwupScrollPlugin({
-            animateScroll: true
-        })
-    ]
+            animateScroll: true,
+        }),
+    ],
 };
 
 const swup = new Swup(options);
 
 let scrollValues = {};
 
-swup.on('clickLink', () => {
+swup.on("clickLink", () => {
     scrollValues[window.location.href] = window.scrollY;
 });
 
-swup.on('popState', () => {
-    setTimeout(function() {
+swup.on("popState", () => {
+    setTimeout(function () {
         window.scrollTo(0, scrollValues[window.location.href]);
     }, 100);
 });
 
 swup.on("contentReplaced", () => {
     hljs.highlightAll();
-    load_cards();
-})
+    init_on_page("experience", init_splide);
+    init_on_page("experience", load_cards);
+});
+
+// Splide.JS code
+function init_splide() {
+    // Init splide
+    var splide = new Splide(".splide", {
+        type: "loop",
+        perPage: 2,
+        perMove: 1,
+        gap: 20,
+        breakpoints: {
+            480: {
+                perPage: 1,
+            },
+        },
+    });
+
+    splide.mount();
+}
+
+// Helper function to call function if on page
+function init_on_page(page_name, func) {
+    // Check if current page is specified page
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    if (page !== page_name) return;
+
+    // Call function
+    func();
+}
